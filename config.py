863c263e6970d468ca2fa41be162e7800df00f91
hugocurran <peter@curran.org.uk>
2026-06-26 16:48:27 +0100

    initial project skeleton

---
+++ config.py
@@ -0,0 +1,22 @@
+"""
+Global configuration.
+
+Only edit this file when changing camera or stream settings.
+"""
+
+CAMERA_WIDTH = 1280
+CAMERA_HEIGHT = 720
+CAMERA_FPS = 30
+
+STREAM_WIDTH = CAMERA_WIDTH
+STREAM_HEIGHT = CAMERA_HEIGHT
+
+BITRATE = 2000          # kbps
+
+SRT_PORT = 9000
+
+LOG_LEVEL = "INFO"
+
+APP_NAME = "raspi-hud"
+
+WINDOW_TITLE = "HUD Preview"
